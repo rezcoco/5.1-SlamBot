01f12a50ef921d6956c9ececf90d8837d32a8d53
Rez <77873738+rezcoco@users.noreply.github.com>
2021-09-12 19:14:37 +0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -140,6 +140,7 @@ except:
 try:
     BOT_TOKEN = getConfig('BOT_TOKEN')
     parent_id = getConfig('GDRIVE_FOLDER_ID')
+    TYPE_ACCOUNT = getConfig('TYPE_ACCOUNT')
     DOWNLOAD_DIR = getConfig('DOWNLOAD_DIR')
     if not DOWNLOAD_DIR.endswith("/"):
         DOWNLOAD_DIR = DOWNLOAD_DIR + '/'
